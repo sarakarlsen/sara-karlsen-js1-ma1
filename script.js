783102905076ba9fbbd4be2f583c6d06c941ae3d
sarakarlsen <71293547+sarakarlsen@users.noreply.github.com>
2021-01-24 22:19:42 +0100

    files added

---
+++ script.js
@@ -0,0 +1,32 @@
+// question
+
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// question
+
+
+
+// question
+
+HTMLHeadingElement
+
+// question
+
+headding
+subheading
+
+// question
+
